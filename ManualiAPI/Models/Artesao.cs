196d2a867ff37d4bf4f018c3d58826cb2ca65e75
sucogelad0 <joaoviniciusteo@gmail.com>
2026-09-20 17:12:58 -0300

    add dos models

---
+++ ManualiAPI/Models/Artesao.cs
@@ -0,0 +1,31 @@
+namespace ManualiAPI.Models;
+
+public class Artesao : User
+{
+    private string _cpf;
+
+    public string Cpf
+    {
+        get { return _cpf; }
+        set
+        {
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                throw new ArgumentException("CPF não pode estar vazio");
+            }
+
+            _cpf = value;
+        }
+    }
+
+    public Artesao(
+        string username,
+        string password,
+        string email,
+        string cep,
+        string cpf)
+        : base(username, password, email, cep)
+    {
+        Cpf = cpf;
+    }
+}
