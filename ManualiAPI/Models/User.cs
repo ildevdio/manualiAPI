f0b02640d5420aa8afaaf4d3fd14ef244a550395
Ildevdio <diogoswc@gmail.com>
2026-09-20 15:51:52 -0300

    Add:User models and dto

--- ManualiAPI/Models/User.cs
+++ ManualiAPI/Models/User.cs
@@ -7,5 +7,55 @@ public class User
     private string _username;
     private string _password;
     private string _email;
+
+    public string Username
+    {
+        get { return _username; }
+
+        set
+        {
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                throw new ArgumentException("Nome não pode estar vazio");
+            }
+            _username = value;
+        }
+    }
+
+    public string Password
+    {
+        get { return _password; }
+
+        set
+        {
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                throw new ArgumentException("Senha não pode estar vazia");
+            }
+            _password = value;
+        }
+    }
+
+    public string Email
+    {
+        get { return _email; }
+
+        set
+        {
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                throw new ArgumentException("E-mail não pode estar vazio");
+            }
+        }
+    }
+
+    public User(string username, string password, string email)
+    {
+        this._id = _idCount++;
+        
+        this._username = username;
+        this._password = password;
+        this._email = email;
+    }
     
 }
