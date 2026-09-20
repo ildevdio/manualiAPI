2aa68d88e685d3900ff222151715c334a298ba2d
Ildevdio <diogoswc@gmail.com>
2026-09-20 18:03:45 -0300

    Fix:Fixing a change that i have done by mistake

--- ManualiAPI/DTO/ProductDTO.cs
+++ ManualiAPI/DTO/ProductDTO.cs
@@ -13,7 +13,7 @@ public class UpdateProductDto
 {
     public int Id { get; set; }
     public string Nome { get; set; }
-    public string Descricao { get; set; }
+    public string Descricao { get; set; } = "";
     public decimal Preco { get; set; }
     public int Estoque { get; set; }
     public bool Ativo { get; set; }
